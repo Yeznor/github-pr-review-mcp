583c4eb9ccb96a38d77c7397a86c0b5414b10a33
Yeznor <260065399+Yeznor@users.noreply.github.com>
2026-07-25 18:16:00 -0600

    Add src/types.ts

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export type PermissionMode = "read" | "write";
+
+export interface RateLimitSnapshot {
+  limit?: number | undefined;
+  remaining?: number | undefined;
+  used?: number | undefined;
+  resetAt?: string | undefined;
+  resource?: string | undefined;
+}
+
+export interface PageOptions {
+  perPage: number;
+  maxItems: number;
+}
+
+export interface ToolEnvelope<T> {
+  ok: true;
+  data: T;
+  rateLimit: RateLimitSnapshot;
+}
+
+export type DiffSide = "LEFT" | "RIGHT";
+
+export interface DiffLine {
+  path: string;
+  side: DiffSide;
+  line: number;
+  text: string;
+}
